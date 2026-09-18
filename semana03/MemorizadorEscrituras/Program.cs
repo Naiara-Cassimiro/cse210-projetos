4ebac52b3277e7736328ebb7868eea8a3aa43630
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-09-18 12:20:55 -0300

    Semana 3

--- semana03/MemorizadorEscrituras/Program.cs
+++ semana03/MemorizadorEscrituras/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Scripture> escrituras = new List<Scripture>();
+
         Reference reference = new Reference("Provérbios", 3, 5, 6);
 
         Scripture scripture = new Scripture(reference,
             "Confia no Senhor de todo o teu coração, e não te estribes no teu próprio entendimento. " +
             "Reconhece-o em todos os teus caminhos, e ele endireitará as tuas veredas.");
-
-        Console.WriteLine(scripture.GetDisplayText());
 
         string entrada = "";
 
